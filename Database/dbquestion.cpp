8ea3cb0a604f962833b09d8334df705337f947d3
Arkadiusz Lega <arkadiusz.stanislaw.lega@gmail.com>
2023-02-17 16:45:29 +0100

    Update DbQuestion - findId - code review.

--- Database/dbquestion.cpp
+++ Database/dbquestion.cpp
@@ -45,9 +45,12 @@ Question *DbQuestion::read(int id){
 
 int DbQuestion::findId(QString value, QString answer){
     QSqlQuery query;
-    query.prepare("SELECT * FROM Questions where Value = :value and Answer = :answer");
-    query.bindValue(":value", value);
-    query.bindValue(":answer", answer);
+    query.prepare(SELECT + "* " + FROM + TABLE_QUESTIONS + " " + WHERE +
+                  COLUMN_VALUE + "=:" + COLUMN_VALUE + " " + AND +
+                  COLUMN_ANSWER + "=:" + COLUMN_ANSWER);
+    //query.prepare("SELECT * FROM Questions where Value = :value and Answer = :answer");
+    query.bindValue(":" + COLUMN_VALUE, value);
+    query.bindValue(":" + COLUMN_ANSWER, answer);
     if(query.exec()){
         while(query.next()){
             int idColumn;
