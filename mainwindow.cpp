cbb405df761dd77a310a9222bd6b6d54b8d356fb
Arkadiusz Lega <arkadiusz.stanislaw.lega@gmail.com>
2023-03-14 12:49:01 +0100

    Update main view - added connection between slots and signals - learn and relation view.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,9 +18,13 @@ MainWindow::MainWindow(QWidget *parent)
 
     QObject::connect(ui->quesiton_view,&QuestionView::added_question_to_db,ui->learn_view,&LearnView::added_question_to_db);
     QObject::connect(ui->quesiton_view,&QuestionView::remove_question_from_db,ui->learn_view,&LearnView::removed_question_from_db);
+
     QObject::connect(ui->tag_view,&TagView::added_tag_to_db,ui->learn_view,&LearnView::added_tag_to_db);
     QObject::connect(ui->tag_view,&TagView::updated_tag_from_db,ui->learn_view,&LearnView::update_tag_in_db);
     QObject::connect(ui->tag_view,&TagView::remove_tag_from_db,ui->learn_view,&LearnView::removed_tag_from_db);
+
+    QObject::connect(ui->relation_view,&RelationView::create_relation,ui->learn_view,&LearnView::create_relation);
+    QObject::connect(ui->relation_view,&RelationView::remove_relation,ui->learn_view,&LearnView::remove_relation);
 
     setWindowTitle("Cards");
 }
